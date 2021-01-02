95ea0147443bfa409d527125b46c73e612b2f1ff
Wildemar Silva <wildemar.dd@gmail.com>
2021-01-01 22:05:12 -0300

    Estado e Cidade

---
+++ src/main/java/com/integraservicos/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.integraservicos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.integraservicos.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado,Integer> {
+
+}
