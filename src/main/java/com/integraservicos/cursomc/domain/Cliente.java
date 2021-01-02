af6dc7adc900078ad9feae2e7113357d58f2e3ad
Wildemar Silva <wildemar.dd@gmail.com>
2021-01-02 10:41:52 -0300

    Revert "Endpoint /clientes/{id}"
    This reverts commit 2908744d0b70e8a0008e8a3160fc8a7536ecf269.

--- src/main/java/com/integraservicos/cursomc/domain/Cliente.java
+++ src/main/java/com/integraservicos/cursomc/domain/Cliente.java
@@ -14,7 +14,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.integraservicos.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -31,7 +30,6 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
--- src/main/java/com/integraservicos/cursomc/domain/Endereco.java
+++ src/main/java/com/integraservicos/cursomc/domain/Endereco.java
@@ -9,8 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 @Entity
 public class Endereco implements Serializable{
 	
@@ -26,7 +24,6 @@ public class Endereco implements Serializable{
 	private String bairro;
 	private String cep;
 	
-	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
