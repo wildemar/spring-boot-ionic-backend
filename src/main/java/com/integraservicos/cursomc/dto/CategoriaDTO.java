0fd49eb4dd118f55a20424f394eb853ddfb967ad
Wildemar Silva <wildemar.dd@gmail.com>
2021-01-09 22:08:36 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/integraservicos/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/integraservicos/cursomc/dto/CategoriaDTO.java
@@ -2,6 +2,10 @@ package com.integraservicos.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.integraservicos.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
@@ -9,7 +13,11 @@ public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=80, message="O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
+
 
 	public CategoriaDTO() {
 	}
@@ -24,7 +32,7 @@ public class CategoriaDTO implements Serializable{
 	}
 
 	public void setId(Integer id) {
-		id = id;
+		this.id = id;
 	}
 
 	public String getNome() {
--- src/main/java/com/integraservicos/cursomc/services/CategoriaService.java
+++ src/main/java/com/integraservicos/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.integraservicos.cursomc.domain.Categoria;
+import com.integraservicos.cursomc.dto.CategoriaDTO;
 import com.integraservicos.cursomc.repositories.CategoriaRepository;
 import com.integraservicos.cursomc.services.exceptions.DataIntegrityException;
 import com.integraservicos.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,6 +56,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(),categoriaDTO.getNome());
 	}
 
 }
