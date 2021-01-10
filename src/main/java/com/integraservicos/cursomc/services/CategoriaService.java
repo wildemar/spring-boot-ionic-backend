1f3f0046e7614dfe8139802ee204e20ca8787602
Wildemar Silva <wildemar.dd@gmail.com>
2021-01-09 23:12:09 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/integraservicos/cursomc/services/CategoriaService.java
+++ src/main/java/com/integraservicos/cursomc/services/CategoriaService.java
@@ -36,8 +36,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -60,6 +61,10 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO categoriaDTO) {
 		return new Categoria(categoriaDTO.getId(),categoriaDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 
 }
